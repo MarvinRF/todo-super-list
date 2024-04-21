33ea0d29cdb4fd5dd743dbae39afa7bf25c3b728
MarvinRF <mamavivi@gmail.com>
2024-04-20 22:36:02 -0300

    'Adds Tarefa component to ListaDeTarefas and styles; adds sample tarefas'

--- src/containers/ListaDeTarefas/index.tsx
+++ src/containers/ListaDeTarefas/index.tsx
@@ -1,12 +1,47 @@
-import React from 'react'
+import Tarefa from '../../components/Tarefa'
+import * as enums from '../../utils/enums/Tarefa'
+import * as S from './style'
+
+const tarefas = [
+  {
+    titulo: 'TRABALHAR',
+    descricao: 'Ver a aula 3',
+    prioridade: enums.Prioridade.IMPORTANTE,
+    status: enums.Status.PENDENTE
+  },
+  {
+    titulo: 'TRABALHAR',
+    descricao: 'Ver a aula 3',
+    prioridade: enums.Prioridade.URGENTE,
+    status: enums.Status.CONCLUIDA
+  },
+  {
+    titulo: 'TRABALHAR',
+    descricao: 'Ver a aula 3',
+    prioridade: enums.Prioridade.NORMAL,
+    status: enums.Status.PENDENTE
+  }
+]
 
 export const ListaDeTarefas = () => {
   return (
-    <main>
+    <S.Container>
       <p>
         2 tarefas marcadas como: &quot;categoria&ldquo; e &quot;termo&ldquo;
       </p>
-    </main>
+      <ul>
+        {tarefas.map((t) => (
+          <li key={t.titulo}>
+            <Tarefa
+              descricao={t.descricao}
+              titulo={t.titulo}
+              status={t.status}
+              prioridade={t.prioridade}
+            />
+          </li>
+        ))}
+      </ul>
+    </S.Container>
   )
 }
 
